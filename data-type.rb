5b635e7257844fa51405a375a9f3485cadcb2446
齐文豪 <dawangong@126.com>
2020-10-16 16:54:04 +0800

    ruby basic - 2

--- data-type.rb
+++ data-type.rb
@@ -2,10 +2,6 @@
 num = 1;
 # string
 str = "str";
-# boolean
-isTruth = false;
-# undefined
-un = nil
 # array
 arr = [0, 1, "2"]
 # hash
@@ -14,8 +10,17 @@ hash = {:a => 1, :b => 2}
 range = 1..5
 
 # 自动换行
-p num, str, isTruth, arr, hash;
+p num, str, arr, hash;
 # 换行
-puts num, str, isTruth, arr, hash;
+puts num, str, arr, hash;
 # 不换行
 print num, str, isTruth, arr, hash;
+
+
+# 伪变量
+# self: 当前方法的接收器对象。
+# true: 代表 true 的值。
+# false: 代表 false 的值。
+# nil: 代表 undefined 的值。
+# __FILE__: 当前源文件的名称。
+# __LINE__: 当前行在源文件中的编号。
